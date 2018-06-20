5c3302d7b933c34db552771818e3eb6576ab70fb
apple <Fuad@apples-MacBook.local>
2018-06-20 22:49:33 +0100

    Implemented RxSwift and completed chapter.

--- Combinestagram/MainViewController.swift
+++ Combinestagram/MainViewController.swift
@@ -25,31 +25,69 @@ import RxSwift
 
 class MainViewController: UIViewController {
 
-  @IBOutlet weak var imagePreview: UIImageView!
-  @IBOutlet weak var buttonClear: UIButton!
-  @IBOutlet weak var buttonSave: UIButton!
-  @IBOutlet weak var itemAdd: UIBarButtonItem!
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-
-  }
-  
-  @IBAction func actionClear() {
-
-  }
-
-  @IBAction func actionSave() {
-
-  }
-
-  @IBAction func actionAdd() {
-
-  }
-
-  func showMessage(_ title: String, description: String? = nil) {
-    let alert = UIAlertController(title: title, message: description, preferredStyle: .alert)
-    alert.addAction(UIAlertAction(title: "Close", style: .default, handler: { [weak self] _ in self?.dismiss(animated: true, completion: nil)}))
-    present(alert, animated: true, completion: nil)
-  }
+    @IBOutlet weak var imagePreview: UIImageView!
+    @IBOutlet weak var buttonClear: UIButton!
+    @IBOutlet weak var buttonSave: UIButton!
+    @IBOutlet weak var itemAdd: UIBarButtonItem!
+    
+    private let bag = DisposeBag()
+    private let images = Variable<[UIImage]>([])
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        images.asObservable().subscribe(onNext: { [weak self] photos in
+            guard let preview = self?.imagePreview else { return }
+            preview.image = UIImage.collage(images: photos, size: preview.frame.size)
+        }).disposed(by: bag)
+        
+        images.asObservable().subscribe(onNext: { [weak self] photos in
+            self?.updateUI(photos: photos)
+        }).disposed(by: bag)
+    }
+    
+    @IBAction func actionClear() {
+        images.value = []
+    }
+    
+    @IBAction func actionSave() {
+        guard let image = self.imagePreview.image else { return }
+        
+        PhotoWriter.save(image).asSingle().subscribe(onSuccess: { [weak self] id in
+            self?.showMessage("Saved with id: \(id)")
+            self?.actionClear()
+            }, onError: { [weak self] error in
+                self?.showMessage("Error", description: error.localizedDescription)
+        }).disposed(by: bag)
+    }
+    
+    @IBAction func actionAdd() {
+        let photosViewController = storyboard!.instantiateViewController(
+            withIdentifier: "PhotosViewController") as! PhotosViewController
+        
+        photosViewController.selectedPhotos.subscribe(onNext: { [weak self] (newImage) in
+            guard let images = self?.images else { return }
+            images.value.append(newImage)
+        }, onDisposed: {
+            print("Completed Photo Selection!")
+        }).disposed(by: bag)
+        
+        navigationController!.pushViewController(photosViewController, animated:
+            true)
+        
+        //images.value.append(UIImage(named: "IMG_1907.jpg")!)
+    }
+    
+    func showMessage(_ title: String, description: String? = nil) {
+        let alert = UIAlertController(title: title, message: description, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Close", style: .default, handler: { [weak self] _ in self?.dismiss(animated: true, completion: nil)}))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    private func updateUI(photos: [UIImage]) {
+        buttonSave.isEnabled = photos.count > 0 && photos.count % 2 == 0
+        buttonClear.isEnabled = photos.count > 0
+        itemAdd.isEnabled = photos.count < 6
+        title = photos.count > 0 ? "\(photos.count) photos" : "Collage"
+    }
 }
